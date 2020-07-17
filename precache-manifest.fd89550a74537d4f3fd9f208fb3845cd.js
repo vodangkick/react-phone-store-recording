18f0d2b769fe68078c92de1647b984a96ae5c400
vodangkick <quang115thd@gmail.com>
2020-07-17 07:19:21 -0700

    Updates

---
+++ precache-manifest.fd89550a74537d4f3fd9f208fb3845cd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a408719b3e69ebe26074525ebbf37d",
+    "url": "/react-phone-store-recording/index.html"
+  },
+  {
+    "revision": "1e729f0a471ee7c287cb",
+    "url": "/react-phone-store-recording/static/css/main.4b1bf43c.chunk.css"
+  },
+  {
+    "revision": "a39716c40fd29009b029",
+    "url": "/react-phone-store-recording/static/js/2.0eb9d11f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-phone-store-recording/static/js/2.0eb9d11f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e729f0a471ee7c287cb",
+    "url": "/react-phone-store-recording/static/js/main.e4bd357d.chunk.js"
+  },
+  {
+    "revision": "82ed36e698f324259c65",
+    "url": "/react-phone-store-recording/static/js/runtime-main.e9c74b72.js"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/react-phone-store-recording/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/react-phone-store-recording/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/react-phone-store-recording/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/react-phone-store-recording/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/react-phone-store-recording/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/react-phone-store-recording/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/react-phone-store-recording/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/react-phone-store-recording/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/react-phone-store-recording/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/react-phone-store-recording/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/react-phone-store-recording/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/react-phone-store-recording/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/react-phone-store-recording/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/react-phone-store-recording/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/react-phone-store-recording/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-phone-store-recording/static/media/logo.5d5d9eef.svg"
+  }
+]);
